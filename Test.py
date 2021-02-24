61415324517d5e36a4b0a99abbeeba09e75a1c33
Ryan Langston <rjameslangston@icloud.com>
2021-02-24 19:13:00 +0000

    removed useless stuff, added to the test game

--- Test.py
+++ Test.py
@@ -1,16 +1,23 @@
-import pytest
 import random
+import pygradle
+
+
+#This is for sneaking
+def stealth():
+    sneak = random.randint(1, 5)
+    if sneak == [1 or 2 or 3 or 4 or 5]:
+        print(sneak)
+
+
 print("A guard stands before you")
 print("you can sneak, attack, or run")
 print("type your response")
-option = input(">>>")
+option = input(">")
 if option == ("sneak"):
     print("You sneak past")
 if option == ("attack"):
-  print("You would attack, but it's not implemented")
+    print("You would attack, but it's not implemented")
 if option == ("run"):
-  print("You run away, flailing your arms, while screaming like a coward")
-#This is for sneaking
-def sneak():
-  sneak = randint(1, 10)
-  if sneak = 
+    print("You run away, flailing your arms, while screaming like a coward")
+if option == ("test"):
+    stealth()
