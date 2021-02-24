4cf4d9205e185b9f46fa5bb041ed250c2785cf7b
Ryan Langston <rjameslangston@icloud.com>
2021-02-24 14:54:46 +0000

    added src and other stuff

--- Test.py
+++ Test.py
@@ -10,3 +10,7 @@ if option == ("attack"):
   print("You would attack, but it's not implemented")
 if option == ("run"):
   print("You run away, flailing your arms, while screaming like a coward")
+#This is for sneaking
+def sneak():
+  sneak = randint(1, 10)
+  if sneak = 
