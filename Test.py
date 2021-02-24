a7c355b3819528f3a704a4b951d9a03a3499617b
Ryan Langston <rjameslangston@icloud.com>
2021-02-24 20:27:18 +0000

    fully implemented stealth and fixed bugs with it

--- Test.py
+++ Test.py
@@ -1,12 +1,15 @@
 import random
-import pygradle
 
 
 #This is for sneaking
 def stealth():
-    sneak = random.randint(1, 5)
-    if sneak == [1 or 2 or 3 or 4 or 5]:
-        print(sneak)
+  sneak = random.randint(1, 10)
+  if sneak == (1 or 2 or 3 or 4 or 5):
+    print("You sneak past")
+  elif sneak:
+    print("You don't sneak past and are caught,")
+    print("This would initate combat but its not done")
+    
 
 
 print("A guard stands before you")
@@ -14,7 +17,7 @@ print("you can sneak, attack, or run")
 print("type your response")
 option = input(">")
 if option == ("sneak"):
-    print("You sneak past")
+    stealth()
 if option == ("attack"):
     print("You would attack, but it's not implemented")
 if option == ("run"):
