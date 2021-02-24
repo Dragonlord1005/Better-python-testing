f004d551b6925018babc96e4035a3d46ebf15728
Ryan Langston <rjameslangston@icloud.com>
2021-02-24 21:50:58 +0000

    fixed stuff

--- Test.py
+++ Test.py
@@ -3,13 +3,12 @@ import random
 
 #This is for sneaking
 def stealth():
-  sneak = random.randint(1, 10)
-  if sneak == (1 or 2 or 3 or 4 or 5):
-    print("You sneak past")
-  elif sneak:
-    print("You don't sneak past and are caught,")
-    print("This would initate combat but its not done")
-    
+    sneak = random.randint(1, 10)
+    if sneak == (1 or 2 or 3 or 4 or 5):
+        print("You sneak past")
+    elif sneak:
+        print("You don't sneak past and are caught,")
+        print("This would initate combat but its not done")
 
 
 print("A guard stands before you")
