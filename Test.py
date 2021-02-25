8bf084de7cb0ac0f4f293b3f8278068e6ced66aa
Ryan Langston <rjameslangston@icloud.com>
2021-02-25 05:21:32 +0000

    most likely changed stuff

--- Test.py
+++ Test.py
@@ -1,10 +1,10 @@
 import random
-
+import os
 
 #This is for sneaking
 def stealth():
     sneak = random.randint(1, 10)
-    if sneak == (1 or 2 or 3 or 4 or 5):
+    if sneak == (1 or 2 or 3 or 4 or 5 or 6 or 7):
         print("You sneak past")
     elif sneak:
         print("You don't sneak past and are caught,")
