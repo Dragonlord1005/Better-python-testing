4207b7e37884abf57282624989f710688d7662c5
Ryan Langston <rjameslangston@icloud.com>
2021-03-02 21:54:26 +0000

    added typing effect

--- examples/attack.py
+++ examples/attack.py
@@ -1,4 +1,18 @@
 # This is purely for testing purposes and may not be final
+import time
+import sys
+
+
+#this is for typing effect
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+
+
 player_health = 50
 player_attack = 7
 enemy_health = 50
@@ -7,13 +21,13 @@ enemy_attack = 7
 def attack():
   global enemy_health
   enemy_health = enemy_health - 7
-  print("you successfully attacked")
-  print("the enemy is now at")
-  print(enemy_health)
+  typingPrint("you successfully attacked\n")
+  print("the enemy is now at", enemy_health, "health\n")
+  
   
 
-print("You can attack or defend")
-print("defending is not implemented")
+typingPrint("You can attack or defend\n")
+typingPrint("defending is not implemented\n")
 battle = input(">")
 
 if battle == ("attack"):
