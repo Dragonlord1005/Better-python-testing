820746cb0d0c1d5ac48dd14b84bc1830da999290
Ryan Langston <rjameslangston@icloud.com>
2021-03-02 20:19:08 +0000

    added player attacking

--- examples/attack.py
+++ examples/attack.py
@@ -3,3 +3,17 @@ player_health = 50
 player_attack = 7
 enemy_health = 50
 enemy_attack = 7
+
+def attack():
+  global enemy_health
+  enemy_health = enemy_health - 7
+  print("you successfully attacked")
+  print(enemy_health)
+  
+
+print("You can attack or defend")
+print("defending is not implemented")
+battle = input(">")
+
+if battle == ("attack"):
+  attack()
