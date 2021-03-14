3f6ab85149e703d8d62dddedfa81e2b5717aa1aa
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2021-03-14 12:10:24 -0600

    removed hello because it was useless

--- hello.py
+++
@@ -1,7 +0,0 @@
-print("Hi! Its me, your friend.")
-print("this is a pretty simple test")
-print("all I have to do is figure out how to give you options")
-print("now for whatever dumb reason, atom hates me, so I cant push these changes until I figure this out, so I'll be right back")
-print("so I figured this dumb thing out")
-print("Now I have to figure out what I want to do with this whole thing")
-print("Alright I figured it out")
