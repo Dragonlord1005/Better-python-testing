e622e1f44a7558b6311bb67b35ede8033b3bacbd
Ryan Langston <>
2021-04-06 19:04:10 +0000

    one day, autoyapf will work not today though

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import time
 import sys
 
 # This is for combat
- player_health = 50
- player_attack = 7
+player_health = 50
+player_attack = 7
 player_defending = 0
 enemy_health = 50
 enemy_attack = 7
