b97b7c4cba3b59e4f45621b26f72c32d2e49eae6
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2021-04-07 17:37:02 -0600

    Update attack.py

--- examples/attack.py
+++ examples/attack.py
@@ -1,5 +1,26 @@
 # this is the combat module
 import random
+import sys
+import time
+
+
+# This is for typing
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def typingInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    value = input()
+    return value
+
+
 
 # This is for combat
 player_health = 50
