b609d81de7845762c12ad47027a0a0b3b6141689
Dragonlord1005 <rjameslangston@icloud.com>
2021-04-10 14:57:31 -0600

    started changing variables to new format

--- examples/attack.py
+++ examples/attack.py
@@ -15,26 +15,27 @@ info = {
   "winner": 3
 }
 win = info["winner"]
+
 while win == 0:
   # this is for player attacking
   def attack():
       enemy_defending = info["enemy_defending"]
-    if info["enemy_defending"] = 0:
+      if info["enemy_defending"] == 0:
         print("hey")
-    dlif enemy_defending == 1:
-      enemy_health = enemy_health - (player_attack / 2)
-      enemy_defending = 0
-    turn = 1
-    print("you successfully attacked")
-    print("the enemy is now at", enemy_health, "health!")
+      elif enemy_defending == 1:
+          info["enemy_health"] = info["enemy_health"] - info["player_attack"] / 2
+          info["enemy_defending"] = 0
+      info["turn"] = 1
+      print("you successfully attacked")
+      print("the enemy is now at", info["enemy_health"], "health!")
 
   # this is for player defending
   def defend():
-    turn = 1
+    info["turn"] = 1
 
-    if turn == 0:
+    if info["turn"] == 0:
         print("You can attack or defend")
-            battle = input(">")
+        battle = input(">")
         if battle == ("attack"):
             attack()
         if battle == ("defend"):
@@ -44,24 +45,19 @@ while win == 0:
         if battle == ("win"):
             winner = 5
         # this is for the enemys turn
-        if turn == 1:
-            global enemy_choice
-            global enemy_defending
-            global enemy_attack
-            global player_health
-            global player_defending
-            enemy_choice = random.randint(1, 10)
-            print("works")
-            if enemy_choice == (1 or 2 or 3 or 4 or 5):
-                print("the enemy is going to attack you!")
-                if player_defending == 0:
-                    player_health = player_health - enemy_attack
-                elif player_defending == 1:
-                    player_health = player_health - (enemy_attack / 2)
-                    player_defending = 0
-            elif enemy_choice:
-                enemy_defending = 1
-                print("The enemy is defending\n")
-            print("You are now at", player_health, "health!!!!")
-            turn = 0
+    if info["turn"] == 1:
+        info["enemy_choice"] = random.randint(1, 10)
+        print("works")
+        if info["enemy_choice"] == (1 or 2 or 3 or 4 or 5):
+            print("the enemy is going to attack you!")
+            if info["player_defending"] == 0:
+                player_health = player_health - enemy_attack
+            elif info["player_defending"] == 1:
+                info["player_health"] = info["player_health"] - info["enemy_attack"] / 2
+                player_defending = 0
+            elif info["enemy_choice"]:
+                info["enemy_defending"] = 1
+                print("The enemy is defending")
+            print("You are now at", info["player_health"], "health!!!!")
+            info["turn"] = 0
 info["winner"] = 0
