8932c311d10c17389eac65c48f6df86c21f05514
Dragonlord1005 <rjameslangston@icloud.com>
2021-04-11 12:34:15 -0600

    changed many things

--- examples/attack.py
+++ examples/attack.py
@@ -1,6 +1,7 @@
 # this is the combat module
 from numba import jit, njit
-import random
+import random,time,sys
+
 
 # This is for combat
 info = {
@@ -12,56 +13,56 @@ info = {
     "enemy_defending": 0,
     "turn": 0,
     "enemy_choice": 0,
-    "winner": 2
+    "winner": 0
 }
-win = info["winner"]
+def combat():
+    info["winner"] = 3
+    while info["winner"] == 3:
+        # this is for player attacking
+        def attack():
+            enemy_defending = info["enemy_defending"]
+            if enemy_defending == 0:
+                print("hey")
+            elif enemy_defending == 1:
+                info["enemy_health"] = info[
+                    "enemy_health"] - info["player_attack"] / 2
+                info["enemy_defending"] = 0
+            info["turn"] = 1
+            print("you successfully attacked")
+            print("the enemy is now at", info["enemy_health"], "health!")
 
-while info["winner"] <= 3:
-    # this is for player attacking
-    def attack():
-        enemy_defending = info["enemy_defending"]
-        if enemy_defending == 0:
-            print("hey")
-        elif enemy_defending == 1:
-            info["enemy_health"] = info[
-                "enemy_health"] - info["player_attack"] / 2
-            info["enemy_defending"] = 0
-        info["turn"] = 1
-        print("you successfully attacked")
-        print("the enemy is now at", info["enemy_health"], "health!")
+        # this is for player defending
+        def defend():
+            info["turn"] = 1
 
-    # this is for player defending
-    def defend():
-        info["turn"] = 1
+        if info["turn"] == 0:
+            print("You can attack or defend")
+            battle = input(">")
+            if battle == ("attack"):
+                attack()
+            if battle == ("defend"):
+                defend()
+            if battle == ("debug"):
+                turn = 1
+            if battle == ("win"):
+                info["winner"] = 5
+                # this is for the enemys turn
+        if info["turn"] == 1:
+            info["enemy_choice"] = random.randint(1, 10)
+            print("works")
+            if info["enemy_choice"] == (1 or 2 or 3 or 4 or 5):
+                print("the enemy is going to attack you!")
+                if info["player_defending"] == 0:
+                    info["player_health"] = ["player_health"
+                                            ] - info["enemy_attack"]
+                elif info["player_defending"] == 1:
+                    info["player_health"] = info[
+                        "player_health"] - info["enemy_attack"] / 2
+                    indo["player_defending"] = 0
+            elif info["enemy_choice"]:
+                info["enemy_defending"] = 1
+                print("The enemy is defending")
+            print("You are now at", info["player_health"], "health!!!!")
+            info["turn"] = 0
 
-    if info["turn"] == 0:
-        print("You can attack or defend")
-        battle = input(">")
-        if battle == ("attack"):
-            attack()
-        if battle == ("defend"):
-            defend()
-        if battle == ("debug"):
-            turn = 1
-        if battle == ("win"):
-            info["winner"] = 5
-            # this is for the enemys turn
-    if info["turn"] == 1:
-        info["enemy_choice"] = random.randint(1, 10)
-        print("works")
-        if info["enemy_choice"] == (1 or 2 or 3 or 4 or 5):
-            print("the enemy is going to attack you!")
-            if info["player_defending"] == 0:
-                info["player_health"] = ["player_health"
-                                         ] - info["enemy_attack"]
-            elif info["player_defending"] == 1:
-                info["player_health"] = info[
-                    "player_health"] - info["enemy_attack"] / 2
-                indo["player_defending"] = 0
-        elif info["enemy_choice"]:
-            info["enemy_defending"] = 1
-            print("The enemy is defending")
-        print("You are now at", info["player_health"], "health!!!!")
-        info["turn"] = 0
-
-info["winner"] = 3
+combat()
