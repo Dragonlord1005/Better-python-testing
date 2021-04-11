ab95da9f8c6f907f8b1fb631579ce39bcccf03d3
Dragonlord1005 <rjameslangston@icloud.com>
2021-04-11 17:38:08 -0600

    deepsource just stop

--- test.py
+++ test.py
@@ -74,7 +74,7 @@ def combat():
 
 # This is for sneaking
 def stealth():
-    sneak = random.randint(1, 20)
+    sneak = randint(1, 20)
     if sneak == (12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20):
         print("You sneak past")
     elif sneak:
