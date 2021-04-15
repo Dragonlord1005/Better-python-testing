8d79dafd5f9555f7cd060b11bd3330458d2faa51
Ryan Langston <>
2021-04-15 17:59:15 +0000

    fixed

--- examples/game.py
+++ examples/game.py
@@ -1,8 +1,2 @@
 # This is a experimental pygame version of everything, don't use this until this comment changes, it is prone to breakpoint
-import pygame as pg
-import pygame_menu
-import pygame_gui
-
-pg.init()
-pygame_menu.Menu
-pygame_gui.elements.UI
+print("go away")
