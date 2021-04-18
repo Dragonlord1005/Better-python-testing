5964b054c69bdf2296fe545e5b25109e6729631d
Dragonlord1005 <rjameslangston@icloud.com>
2021-04-18 14:43:59 -0600

    added setting health
    This allows for you to set the health easily

--- examples/stats.py
+++ examples/stats.py
@@ -13,6 +13,8 @@ class player:
 
     def get_health(self):
         return self.health
+    def set_health(self, health):
+        self.health = health
 
 
 Jack = player("Jack", 37, 50)
@@ -26,3 +28,6 @@ info = input(">")
 character_age = info
 player_info = player(character_name, character_age, 50)
 print(player_info.get_name(), player_info.get_age(), player_info.get_health())
+
+player_info.set_health(8)
+print(player_inf0.get_health)
