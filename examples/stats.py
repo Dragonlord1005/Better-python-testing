faab29ac43654be0e95af6b33cb878a41c421046
Dragonlord1005 <github_user_name_of_maintainer@users.noreply.github.com>
2021-04-18 18:45:11 +0000

    Automated autoyapf fixes

--- examples/stats.py
+++ examples/stats.py
@@ -1,15 +1,15 @@
 # This is a new version of stats from the combat but instead it uses classes which I hope will make things easier
 class player:
-    
     def __init__(self, name, health):
         self.name = name
         self.health = health
+
     def get_name(self):
         return self.name
+
     def get_health(self):
         return self.health
 
 
 Jack = player("Jack", 50)
 print(Jack.get_name(), Jack.get_health())
-
