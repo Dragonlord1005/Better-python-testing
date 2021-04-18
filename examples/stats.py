57c2a9fb26ae2d4be63d642e579f6916bd813bbb
Dragonlord1005 <rjameslangston@icloud.com>
2021-04-18 13:22:23 -0600

    added health

--- examples/stats.py
+++ examples/stats.py
@@ -1,8 +1,9 @@
 # This is a new version of stats from the combat but instead it uses classes which I hope will make things easier
 class player:
-    def __init__(self, name, age):
+    def __init__(self, name, age, health):
         self.name = name
         self.age = age
+        self.health = health
 
     def get_name(self):
         return self.name
@@ -10,8 +11,11 @@ class player:
     def get_age(self):
         return self.age
 
+    def get_health(self):
+        return self.health
 
-Jack = player("Jack", 50)
+
+Jack = player("Jack", 37, 50)
 print(Jack.get_name(), Jack.get_age())
 
 print("What is your name?")
@@ -20,5 +24,5 @@ character_name = info
 print("What is your age?")
 info = input(">")
 character_age = info
-player_info = player(character_name, character_age)
-print(player_info.get_name(), player_info.get_age())
+player_info = player(character_name, character_age, 50)
+print(player_info.get_name(), player_info.get_age(), player_info.get_health())
