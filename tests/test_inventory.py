6716ad522d92337c81a77635319e9c84183fda6f
Dragonlord1005 <>
2021-04-26 16:42:28 +0000

    yep

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -7,8 +7,8 @@ class object():
         self.weight = weight
         self.price = price
 
-        def get_name(self):
-            return self.name
+    def get_name(self):
+      return self.name
 
-        def get_weight(self):
-            return self.weight
+      def get_weight(self):
+        return self.weight
