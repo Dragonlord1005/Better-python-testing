a69a96a8568d5b240d02c083adba246b9263df81
Dragonlord1005 <>
2021-04-28 17:54:26 +0000

    removed useless stuff

--- game.py
+++ game.py
@@ -10,14 +10,6 @@ def typingprint(text):
         time.sleep(0.05)
 
 
-def typinginput(text):
-    for character in text:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.05)
-    value = input()
-    return value
-
 
 # This is for combat
 info = {
--- gametest.py
+++ gametest.py
@@ -10,13 +10,6 @@ def typingprint(text):
         time.sleep(0.03)
 
 
-def typinginput(text):
-    for character in text:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.05)
-    value = input()
-    return value
 
 
 # This is for combat
