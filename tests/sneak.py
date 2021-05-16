4a680dbd5a37ae5d6decf10ee83d4dba07125fbb
Dragonlord1005 <rjameslangston@icloud.com>
2021-05-16 13:35:28 -0600

    changed the way things tests are done

---
+++ tests/sneak.py
@@ -0,0 +1,11 @@
+# This is for sneaking
+import random
+
+
+def stealth():
+    sneak = random.randint(1, 10)
+    if sneak == (1 or 2 or 3 or 4 or 5):
+        print("You sneak past")
+    elif sneak:
+        print("You don't sneak past and are caught,")
+        print("this does nothing in the example")
