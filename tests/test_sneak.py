73e23c86dddc95e76b750112a7d1b950105a4dc5
Dragonlord1005 <rjameslangston@icloud.com>
2021-05-16 19:08:18 -0600

    fixed deeosource

--- tests/test_sneak.py
+++ tests/test_sneak.py
@@ -3,8 +3,7 @@ import random
 
 
 def stealth():
-    """[Defines stealth, needs no additional arguments.]
-    """
+    """[Defines stealth, needs no additional arguments.]"""
     sneak = random.randint(1, 10)
     if sneak == (1 or 2 or 3 or 4 or 5):
         print("You sneak past")
