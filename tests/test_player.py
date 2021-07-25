34ebd0fe748b214c5d455f940893c2a2d6c2bc55
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2021-07-25 13:46:46 -0600

    Many things

--- tests/test_player.py
+++ tests/test_player.py
@@ -3,3 +3,11 @@ class Player:
         """Inititates stuff"""
         self.gold = gold
         self.health = health
+
+    def get_gold(self):
+        return self.gold
+
+
+def test_player():
+    joe = Player(1, 20)
+    assert print(joe.get_gold()) != 1
