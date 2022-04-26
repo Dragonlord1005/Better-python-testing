02b7b53f4a4c6e9c8712145cc19805ef9dc4a693
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-04-26 15:01:25 +0000

    chore(format): Formatted engine.py

--- roguelike/engine.py
+++ roguelike/engine.py
@@ -7,8 +7,11 @@ from actions import EscapeAction, MovementAction
 from entity import Entity
 from input_handlers import EventHandler
 
+
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
+    def __init__(
+        self, entities: Set[Entity], event_handler: EventHandler, player: Entity
+    ):
         self.entities = entities
         self.event_handler = event_handler
         self.player = player
