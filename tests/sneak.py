9442eb95c0921dbe7717000a3618f7bf6ef0b23e
autoblack <68302450+Dragonlord1005@users.noreply.github.com>
2022-05-27 23:47:43 +0000

    fixup: Format Python code with Black

--- tests/sneak.py
+++ tests/sneak.py
@@ -1,8 +1,8 @@
 def stealth():
     """Defines stealth, needs no additional arguments."""
-    sneak = random.randint(1, 10) 
+    sneak = random.randint(1, 10)
     if sneak == (1 or 2 or 3 or 4 or 5):
         print("You sneak past")
     elif sneak:
         print("You don't sneak past and are caught,")
-        print("this does nothing in the example") 
+        print("this does nothing in the example")
