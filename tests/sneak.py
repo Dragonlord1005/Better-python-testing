2ecc6da776b82fbd3d1fe2a7bfa868493386db87
Ryan Langston <drago@The-Dragon.localdomain>
2022-05-27 17:47:10 -0600

    Awesome

--- tests/sneak.py
+++ tests/sneak.py
@@ -1,12 +1,8 @@
-# This is for sneaking
-import random
-
-
 def stealth():
-    """Defines stealth"""
-    sneak = random.randint(1, 10)
+    """Defines stealth, needs no additional arguments."""
+    sneak = random.randint(1, 10) 
     if sneak == (1 or 2 or 3 or 4 or 5):
         print("You sneak past")
     elif sneak:
         print("You don't sneak past and are caught,")
-        print("this does nothing in the example")
+        print("this does nothing in the example") 
